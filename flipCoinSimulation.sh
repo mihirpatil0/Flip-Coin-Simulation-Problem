612284ebe7e36c1d6fb35895f5a329e0d9816587
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 18:00:58 +0530

    head or tail as winner

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 echo "Welcome to Flip Coin Simulation - This problem displays the winner Heads or Tails"
+
+function Case()
+{
+	case $1 in
+		1) echo "HEAD";;
+		0) echo "TAIL";;
+	esac
+}
+
+RandomOne=$((RANDOM%2))
+echo "Player Chooses : `Case $RandomOne`"
+RandomTwo=$((RANDOM%2))
+echo "Fliped Coin Shows : `Case $RandomTwo`"
+
+if [ $RandomOne == $RandomTwo ]
+then
+	echo "Player : Wins"
+else
+	echo "Player : Loose"
+fi
